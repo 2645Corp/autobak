1fefdeaa9690a7bd625eaf94a9cd72b7ada8caf9
LI JIAHAO <lijiahao99131@gmail.com>
2017-03-28 18:58:22 -0500

    fix conf source problem

--- do.sh
+++ do.sh
@@ -11,8 +11,16 @@ echo ""
 echo "***********************"
 echo "Autobak task started at "`date`
 
-source ./autobak.conf
 
+SOURCE="$0"
+while [ -h "$SOURCE"  ]; do # resolve $SOURCE until the file is no longer a symlink
+	    DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+		SOURCE="$(readlink "$SOURCE")"
+		[[ $SOURCE != /*  ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+
+source "$DIR/autobak.conf"
 # Dump database
 echo "Dumping databases..."
 
